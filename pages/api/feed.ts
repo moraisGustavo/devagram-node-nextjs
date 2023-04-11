0007fb4f19d1660de1e3b8126d62272f340e9694
Gustavo Morais <gustavohms.santos@gmail.com>
2023-04-10 22:32:05 -0300

    Revert "criacao da api de alteracao do usuario"
    This reverts commit 57d1160f1825c60aaf391365d2c74c59d291531f.

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -1,7 +1,7 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
-import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 import {validarTokenJWT } from '../../middlewares/validarTokenJWT';
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
+import { RespostaPadraoMsg } from '@/types/RespostaPadraoMsg';
 import { UsuarioModel } from '@/models/UsuarioModel';
 import { PublicacaoModel } from '@/models/PublicacaoModel';
 
@@ -24,19 +24,13 @@ const feedEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPa
                     .sort({data : -1});
 
                 return res.status(200).json(publicacoes);
-            }else{
-                const {userId} = req.query;
-                const usuarioLogado = await UsuarioModel.findById(userId);
-                if(!usuarioLogado){
-                    return res.status(400).json({erro: 'Usuario nao encontrado'});
-                }
             }
         }
         return res.status(405).json({erro: 'Metodo informado invalido'});
     }catch(e){
         console.log(e);
+        return res.status(400).json({erro: 'Nao foi possivel obter o feed'});
     }
-    return res.status(400).json({erro: 'Nao foi possivel obter o feed'});
 }
 
 export default  validarTokenJWT(conectarMongoDB(feedEndpoint));
--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -1,71 +1,21 @@
-import type {NextApiRequest, NextApiResponse,} from 'next';
-import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
+import type {NextApiRequest, NextApiResponse, NextApiHandler} from 'next';
 import {validarTokenJWT } from '../../middlewares/validarTokenJWT';
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
-import { UsuarioModel } from '../../models/UsuarioModel';
-import nc from 'next-connect';
-import { upload, uploadImagemCosmic } from '@/services/uploadImagemCosmic';
+import { UsuarioModel } from '@/models/UsuarioModel';
 
+const usuarioEndpoint = async (req : NextApiRequest, res : NextApiResponse) => {
+    try{
+        //Pegar a id do usuario na query do token
+        const {userId} = req?.query;
 
-const handler = nc()
-    .use(upload.single('file'))
-    .put(async(req : any, res : NextApiResponse<RespostaPadraoMsg>) =>{
-        try{
-            // Buscar o usuario no BD 
-            const {userId} = req?.query;
-            const usuario = await UsuarioModel.findById(userId);
-
-            //Verifica se o usuario foi encontrado
-            if(!usuario){
-                return res.status(400).json({erro: 'Usuario nao encontrado'});
-            }
-
-            // Buscand o nome no Body
-            const {nome} = req?.body;
-            if(nome && nome.length > 2){
-                usuario.nome = nome;
-            }
-
-            // Buscar a imagem 
-            const {file} = req;
-            if(file && file.originalname){
-                const image = await uploadImagemCosmic(req); // Subir a imagem para o cosmic
-                if(image && image.media && image.media.url){
-                    usuario.avatar = image.media.url; 
-                }
-            }
-
-            // Alterar os dado no db
-            await UsuarioModel
-            .findByIdAndUpdate({_id : usuario._id}, usuario);
-
-            return res.status(200).json({msg: 'Usuario alterado com sucesso'});
-            
-        }catch(e){
-            console.log(e);
-            return res.status(400).json({erro: 'Nao foi possivel atualizar usuario'})
-        }
-    })
-
-    .get(async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any>) => {
-        try{
-            //Pegar a id do usuario na query do token
-            const {userId} = req?.query;
-            //Buscar os dados do usuario no bd
-            const usuario = await UsuarioModel.findById(userId);
-            usuario.senha = null; // para nao retornar a senha do usuario
-            return res.status(200).json(usuario);    
-        }catch(e){
-            console.log(e);
-        }
+        //Buscar os dados do usuario no bd
+        const usuario = await UsuarioModel.findById(userId);
+        usuario.senha = null; // para nao retornar a senha do usuario
+        return res.status(200).json(usuario);    
+    }catch(e){
+        console.log(e);
         return res.status(400).json({erro: 'Nao foi possivel obter dados do usuario'});
-    });
-
-    export const config = {
-        api: {
-            bodyParser: false
-        }
     }
+ }
 
-
-export  default validarTokenJWT(conectarMongoDB(handler));
+export  default validarTokenJWT(conectarMongoDB(usuarioEndpoint));
